ab887e97ace5b5d6646c0e48469d56cbdf63a099
DESKTOP-IG8VVD0\administart <ko.lim20150406@gmail.com>
2026-03-11 22:41:10 +0800

    林臭屁的弟的提交Day20

--- study.sql
+++ study.sql
@@ -19,3 +19,18 @@ create table user(
 ) comment '用户信息表';
 
 
+create table emp(
+    id int unsigned primary key auto_increment comment 'ID',
+    username varchar(20) not null unique comment '用户名',
+    password varchar(32) default '123456' comment '密码',
+    name varchar(10) not null comment '姓名',
+    gender tinyint unsigned not null comment '性别 , 1 男 2 女',
+    phone char(11) not null unique comment '',
+    job tinyint unsigned comment '职位 1 2 3 4 5',
+    salary int unsigned comment '薪资',
+    entry_date date comment '入职日期',
+    image varchar(255) comment '图像',
+    create_time datetime comment '创建时间',
+    update_time datetime comment '修改时间'
+) comment '员工表';
+
