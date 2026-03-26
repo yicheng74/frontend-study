64d7ff62f089eff105dbd93c1e3692c04a099dab
DESKTOP-IG8VVD0\administart <ko.lim20150406@gmail.com>
2026-03-26 21:28:33 +0800

    林臭屁的弟的提交Day30

--- springboot/demo/src/main/java/com/exampleforsb/demo/mapper/DeptMapper.java
+++ springboot/demo/src/main/java/com/exampleforsb/demo/mapper/DeptMapper.java
@@ -4,9 +4,17 @@ import com.exampleforsb.demo.pojo.Dept;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Result;
 @Mapper
 public interface DeptMapper{
 
+
+    /*@Results({
+        @Result(column = "create_time" , property = "createTime"),
+        @Result(column = "update_time" , property = "updateTime")
+
+    })*/
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
 }
