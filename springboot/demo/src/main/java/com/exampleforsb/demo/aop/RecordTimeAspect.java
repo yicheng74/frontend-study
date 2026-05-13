d18ea831b808febf18427208568eedf45a325ee8
DESKTOP-IG8VVD0\administart <ko.lim20150406@gmail.com>
2026-05-13 18:29:17 +0800

    林臭屁的弟的提交Day57

--- springboot/demo/src/main/java/com/exampleforsb/demo/aop/RecordTimeAspect.java
+++ springboot/demo/src/main/java/com/exampleforsb/demo/aop/RecordTimeAspect.java
@@ -4,12 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect // 标识当前类是一个AOP类
 @Component
 public class RecordTimeAspect {
+
+
+    /*@Pointcut("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
+    public void pointcut() {
+    }*/
+
+    /*@Before("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
+    public void before(ProceedingJoinPoint pjp) {
+        log.info("方法执行开始: {}", pjp.getSignature().getName());
+    }*/
 
     @Around("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
     public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
@@ -28,4 +40,19 @@ public class RecordTimeAspect {
 
         return result;
     }
+
+    /*@After("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
+    public void after(ProceedingJoinPoint pjp) {
+        log.info("方法执行结束: {}", pjp.getSignature().getName());
+    }*/
+
+    /*@AfterReturning("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
+    public void afterReturning(ProceedingJoinPoint pjp) {
+        log.info("方法执行返回: {}", pjp.getSignature().getName());
+    }*/
+
+    /*@AfterThrowing("execution(* com.exampleforsb.demo.service.impl.*.*(..))")
+    public void afterThrowing(ProceedingJoinPoint pjp) {
+        log.info("方法执行抛出异常: {}", pjp.getSignature().getName());
+    }*/
 }
