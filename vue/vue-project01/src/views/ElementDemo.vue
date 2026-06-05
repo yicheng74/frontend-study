f4acfbe2b315242b9c87a1e8f621be9b08d5544c
DESKTOP-IG8VVD0\administart <ko.lim20150406@gmail.com>
2026-06-05 21:48:39 +0800

    林臭屁的弟的提交Day62

--- vue/vue-project01/src/views/ElementDemo.vue
+++ vue/vue-project01/src/views/ElementDemo.vue
@@ -1,10 +1,46 @@
 <script setup>
-  
+
+  import { ref } from 'vue'
+
+  const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+  //分页相关
+  const currentPage4 = ref(4)
+  const pageSize4 = ref(100)
+  const background = ref(false)
+  const disabled = ref(false)
+
+  const handleSizeChange = (val) => {
+    console.log(`${val} items per page`)
+  }
+  const handleCurrentChange = (val) => {
+    console.log(`current page: ${val}`)
+  }
 </script>
 
 
 <template>
-  <div class="button-example">
+  <!-- <div class="button-example">
     <div class="button-row">
       <el-button>Default</el-button>
       <el-button type="primary">Primary</el-button>
@@ -22,9 +58,29 @@
       <el-button type="warning" plain>Warning</el-button>
       <el-button type="danger" plain>Danger</el-button>
     </div>
+  </div> 
+  -->
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+
+  <div class="demo-pagination-block">
+    <div class="demonstration">All combined</div>
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
-
 <style scoped>
 
 </style>
